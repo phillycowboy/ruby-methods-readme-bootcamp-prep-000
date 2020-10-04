d84b8c3d345e228d68819a8cf1f4062e6c07974a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-04 21:12:17 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -3,12 +3,12 @@ def say_hello_ten_times
   phrase = "Hello World!"
   puts phrase
   puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
