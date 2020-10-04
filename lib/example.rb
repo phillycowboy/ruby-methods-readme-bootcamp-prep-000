fa99c172b4e80bb770ad550a98a9ba4fa7ed5f48
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-04 21:11:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,8 @@
 # Use this space to code along with the readme
-
-phrase = "Hello World!"
+def say_hello_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
 puts phrase
 puts phrase
 puts phrase
@@ -9,5 +11,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
+end
